f65eb7b4836453b0fbec50d0ea5a37966ade3957
John Ramey <johnramey@gmail.com>
2012-02-22 08:19:17 -0800

    Added the data-generation code for the intraclass covariance structure. Removed the references to the 'foreach' package. Updated the documentation for the 'simdiag' functions. Updated the 'simdiag2' function to accept two matrices rather than a data set; the data set is dealt with in 'simdiag.default'.

--- R/helper-simdiag2.r
+++ R/helper-simdiag2.r
@@ -1,46 +1,51 @@
 #' Simultaneously diagonalizes two real symmetric matrices.
 #'
-#' This function simultaneously diagaonlizes two real symmetric matrices.
+#' This function simultaneously diagonalizes two real symmetric matrices.
 #' If the matrices are singular, we employ a dimension reduction
-#' method that simultaneously diagonalizes them in a lower dimension.
+#' method that simultaneously diagonalizes them to a lower dimension, \code{q}.
 #'
-#' If the user selects the dimension reduction option, the user may
-#' specify a value of k. This is potentially useful if a scree plot
-#' is utilized or a manual threshold is desired. Otherwise, we
-#' select k automatically to be the number of non-zero eigenvalues.
-#' To determine what constitutes 'non-zero', we select all of the eigenvalues
-#' larger than the specified tolerance, 'tol'.
-#'
-#' The eigen_pct is the percentage of eigenvalues to use, by which we mean
-#' the cumulative sum of the eigenvalues (scaled by the sum of the eigenvalues).
-#' This is similar to the typical automatic approach to Principal Components Analysis.
+#' The user can manually provides the dimension, \code{q}. This is potentially
+#' useful if a scree plot is utilized or a manual threshold is desired.
+#' Otherwise, we select \code{q} automatically to be the number of non-zero
+#' eigenvalues. To determine what constitutes 'non-zero', we select all of the
+#' eigenvalues larger than the specified tolerance, \code{tol}. This is similar
+#' to a typical automatic approach to Principal Components Analysis.
 #' 
-#' If the user does not wish to use the dimension reduction method,
-#' then k is automatically computed as the number of eigenvalues of B.
-#' Hence, in this case, k = p.
+#' The returned simultaneous diagonalizing matrix, \code{Q}, is constructed such
+#' that
 #'
-#' The returned simultaneous diagonalizing matrix, Q, is constructed such that
+#' \deqn{Q A Q' = D,}
+#' and
+#' \deqn{Q B Q' = I_q},
 #'
-#' Q' A Q = D, and
-#' Q' B Q = I_k,
-#'
-#' where I_k denotes the k-dimensional identity matrix and Lambda is the 
-#' diagonal matrix with diagonal entries as the characteristic roots
+#' where \eqn{I_q} denotes the \eqn{q}-dimensional identity matrix and \eqn{D} is
+#' the diagonal matrix with diagonal entries as the characteristic roots
 #' (generalized eigenvalues) of det(B - lambda A) = 0.
 #'
 #' @export
 #' @param A p x p real symmetric matrix
 #' @param B p x p real symmetric matrix
-#' @param dim_reduce logical flag: Should we reduce the dimension of the simultaneous diagonalization?
 #' @param q the reduced dimension of the simultaneous diagonalizer.
-#' @param method the method to automatically select the value of \code{q}.
-#' Ignored if \code{q} is given.
 #' @param tol a value indicating the magnitude below which eigenvalues are considered 0.
 #' @return a list containing:
-#'	the matrix Q that is the p x k simultaneous diagonalizing matrix
-#'	the selected value of k
-simdiag2 <- function(x, y, q = NULL, tol = 1e-6) {
-  p <- ncol(x)
+#'	the matrix Q that is the \eqn{p \times q} simultaneous diagonalizing matrix
+#'	the selected value of \code{q}
+simdiag2 <- function(A, B, q = NULL, tol = 1e-6) {
+  A <- as.matrix(A)
+  B <- as.matrix(B)
+  
+  if (nrow(A) != ncol(A)) {
+    stop("The matrix, 'A', must be square.")
+  }
+
+  if (nrow(B) != ncol(B)) {
+    stop("The matrix, 'B', must be square.")
+  }
+  if (nrow(A) != nrow(B)) {
+    stop("The matrices, 'A' and 'B', must have the same size.")
+  }
+  p <- nrow(A)
+
   # If a value for 'q' is provided, we check that it is between 1 and p,
   # inclusively. If not, an error is thrown.
   if (!is.null(q)) {
@@ -49,36 +54,18 @@ simdiag2 <- function(x, y, q = NULL, tol = 1e-6) {
       stop("The value for 'q' must be between 1 and p, inclusively.")
     }
   }
-
-  # For now, we only simultaneously diagonalize the covariance matrices for two
-  # classes.
-  if (nlevels(y) < 2) {
-    stop("Only 1 class is given. Use PCA instead.")
-  } else if (nlevels(y) > 2) {
-    stop("Currently, we cannot simultaneously diagonalize more than 2 classes.")
-  }
-
-  # Computes the MLEs of the covariance matrices for each class
-  # Because we assume rank(Sig1) >= rank(Sig2), we designate class 1 as the class
-  # with the larger sample size, which is our preliminary candidate value for 'q'.
-  larger_class <- which.max(table(y))
-  n1 <- sum(y == larger_class)
-  n2 <- sum(y != larger_class)
-  Sig1 <- (n1 - 1) * cov(x[which(y == larger_class), ]) / n1
-  Sig2 <- (n2 - 1) * cov(x[which(y != larger_class), ]) / n2
-
-  eigen_Sig1 <- eigen(Sig1, symmetric = TRUE)
+  
+  eigen_A <- eigen(A, symmetric = TRUE)
   if (is.null(q)) {
-    q <- sum(eigen_Sig1$values > tol)
+    q <- sum(eigen_A$values > tol)
   }
-  u1 <- eigen_Sig1$vectors[, seq_len(q)]
-  lambda1_inv <- diag(eigen_Sig1$values[seq_len(q)]^(-1/2))
-  Q1 <- tcrossprod(lambda1_inv, u1)
-  diag(Q1 %*% tcrossprod(Sig1, Q1))
+  u_A <- eigen_A$vectors[, seq_len(q)]
+  lambda_A_inv <- diag(eigen_A$values[seq_len(q)]^(-1/2))
+  Q_A <- tcrossprod(lambda_A_inv, u_A)
 
-  eigen_2 <- eigen(Q1 %*% tcrossprod(Sig2, Q1), symmetric = TRUE)
-  u12 <- eigen_2$vectors[, seq_len(q)]
-  Q <- crossprod(u12, Q1)
+  eigen_2 <- eigen(Q_A %*% tcrossprod(B, Q_A), symmetric = TRUE)
+  u_2 <- eigen_2$vectors[, seq_len(q)]
+  Q <- crossprod(u_2, Q_A)
 
   list(Q = Q, q = q)
 }
--- R/simdiag.r
+++ R/simdiag.r
@@ -1,47 +1,56 @@
-#' Simultaneously diagonalizes two real symmetric matrices.
+#' Simultaneously diagonalizes the sample covariance matrices for a data set
+#' generated from \code{K} classes.
 #'
-#' This function simultaneously diagonalizes two real symmetric matrices.
+#' This function determines the matrix, \code{Q}, that simultaneously diagonalizes
+#' the sample covariance matrices for a data set generated from \code{K} classes.
 #' If the matrices are singular, we employ a dimension reduction
-#' method that simultaneously diagonalizes them in a lower dimension.
+#' method that simultaneously diagonalizes them to a lower dimension, \code{q}.
 #'
-#' If the user selects the dimension reduction option, the user may
-#' specify a value of k. This is potentially useful if a scree plot
-#' is utilized or a manual threshold is desired. Otherwise, we
-#' select k automatically to be the number of non-zero eigenvalues.
-#' To determine what constitutes 'non-zero', we select all of the eigenvalues
-#' larger than the specified tolerance, 'tol'.
+#' The user can manually provides the dimension, \code{q}. This is potentially
+#' useful if a scree plot is utilized or a manual threshold is desired.
+#' Otherwise, we select \code{q} automatically to be the number of non-zero
+#' eigenvalues. To determine what constitutes 'non-zero', we select all of the
+#' eigenvalues larger than the specified tolerance, \code{tol}. This is similar
+#' to a typical automatic approach to Principal Components Analysis (PCA).
 #' 
-#' If the user does not wish to use the dimension reduction method,
-#' then k is automatically computed as the number of eigenvalues of B.
-#' Hence, in this case, k = p.
+#' The returned simultaneous diagonalizing matrix, \code{Q}, is constructed such
+#' that
 #'
-#' The returned simultaneous diagonalizing matrix, Q, is constructed such that
+#' \deqn{Q \widehat{\Sigma_k} Q' = D_k}
 #'
-#' Q A Q' = D, and
-#' Q B Q' = I_k,
+#' for all \eqn{k = 1, \ldots, K}, where \eqn{\widehat{\Sigma_k}} is the maximum
+#' likelihood estimator (under normality) for the \eqn{k}th class covariance
+#' matrix, \eqn{\Sigma_k}, where \eqn{D_k} is a diagonal matrix.
 #'
-#' where I_k denotes the k-dimensional identity matrix and Lambda is the 
-#' diagonal matrix with diagonal entries as the characteristic roots
-#' (generalized eigenvalues) of det(B - lambda A) = 0.
+#' The returned simultaneously-diagonalizing matrix, \code{Q}, is of size
+#' \eqn{p \times q}.
+#'
+#' Currently, we only allow \eqn{K = 2}. The \eqn{K = 1} case is equivalent to
+#' PCA. The \eqn{K \ge 2} case is of ongoing research; note that a set of
+#' \code{K} positive semidefinite matrices are simultaneously diagonalizable if
+#' they are pairwise-commute (i.e. \eqn{A B = B A} for any two matrices \eqn{A}
+#' and \eqn{B} in the set). However, pairwise commuting is a strict assumption
+#' that we wish to relax.
 #'
 #' @export
-#' @param A p x p real symmetric matrix
-#' @param B p x p real symmetric matrix
-
+#' @param x matrix containing the training data. The rows are the sample
+#' observations, and the columns are the features.
+#' @param y vector of class labels for each training observation
 #' @param q the reduced dimension of the simultaneous diagonalizer.
 #' @param method the method to automatically select the value of \code{q}.
 #' Ignored if \code{q} is given.
 #' @param simdiag_data Should the data matrix, \code{x}, be transformed and
 #' returned? If TRUE (default), we return an updated matrix, \code{x}. For large
-#' data sets, this option can yield a very large \code{simdiag} object.
+#' data sets, this option can yield a very large \code{simdiag} object and should
+#' possibly be calculated manually.
 #' @param tol a value indicating the magnitude below which eigenvalues are
 #' considered 0.
 #' @return a list containing:
 #' \itemize{
-#'   \item \code{Q}: simultaneous diagonalizing matrix of size p x q
+#'   \item \code{Q}: simultaneous diagonalizing matrix of size \eqn{p \times q}
 #'	 \item \code{q}: the reduced dimension of the data
 #'   \item \code{x}: the transformed data matrix, \code{x}. Returned, if
-#'   simdiag_data is TRUE. Otherwise, \code{x} is not returned.
+#'   \code{simdiag_data} is TRUE. Otherwise, \code{x} is not returned.
 #' }
 simdiag <- function(x, ...)
   UseMethod("simdiag")
@@ -54,12 +63,39 @@ simdiag.default <- function(x, y, q = NULL, method = c("rank", "bhattacharyya"),
   x <- as.matrix(x)
   y <- as.factor(y)
 
+  p <- ncol(x)
+  # If a value for 'q' is provided, we check that it is between 1 and p,
+  # inclusively. If not, an error is thrown.
+  if (!is.null(q)) {
+    q <- as.integer(q)
+    if (q < 1 || q > p) {
+      stop("The value for 'q' must be between 1 and p, inclusively.")
+    }
+  }
+
+  # For now, we only simultaneously diagonalize the covariance matrices for two
+  # classes.
+  if (nlevels(y) < 2) {
+    stop("Only 1 class is given. Use PCA instead.")
+  } else if (nlevels(y) > 2) {
+    stop("Currently, we cannot simultaneously diagonalize more than 2 classes.")
+  }
+
+  # Computes the MLEs of the covariance matrices for each class
+  # Because we assume rank(Sig1) >= rank(Sig2), we designate class 1 as the class
+  # with the larger sample size, which is our preliminary candidate value for 'q'.
+  larger_class <- which.max(table(y))
+  n1 <- sum(y == larger_class)
+  n2 <- sum(y != larger_class)
+  Sig1 <- (n1 - 1) * cov(x[which(y == larger_class), ]) / n1
+  Sig2 <- (n2 - 1) * cov(x[which(y != larger_class), ]) / n2
+
   # In future work, we intend to allow for more than the K = 2 case. Hence, we
   # have delegated the actual simultaneous diagonalization to the function,
   # 'simdiag2'.
   # TODO: Pass 'method' to simdiag2. For now, we are only using the rank
   # estimation to select 'q'.
-  obj <- diagdiscrim:::simdiag2(x = x, y = y, q = q, tol = tol)
+  obj <- diagdiscrim:::simdiag2(A = Sig1, B = Sig2, q = q, tol = tol)
 
   # Creates an object of type 'simdiag' and adds the 'match.call' to the object
   obj$call <- match.call()
