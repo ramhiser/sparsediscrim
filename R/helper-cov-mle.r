8f97fdb41af8544b4ee53ae4a32defce345b46b6
John Ramey <johnramey@gmail.com>
2012-03-24 20:13:15 -0500

    Added a function to compute the pooled covariance matrix MLE.

--- R/helper-cov-mle.r
+++ R/helper-cov-mle.r
@@ -11,3 +11,24 @@
 cov_mle <- function(x) {
   (nrow(x) - 1) / nrow(x) * cov(x)
 }
+
+#' Computes the pooled maximum likelihood estimator for the common covariance
+#' matrix from \eqn{K} multivariate normal populations.
+#'
+#' TODO: Description
+#'
+#' @export
+#' @param x data matrix with \code{n} observations and \code{p} feature vectors
+#' @param y class labels for observations (rows) in \code{x}
+#' @return pooled sample covariance matrix of size \eqn{p \times p}
+#' @examples
+#' cov_pool(iris[,-5], iris$Species)
+cov_pool <- function(x, y) {
+  x <- as.matrix(x)
+  y <- as.factor(y)
+  n <- length(y)
+  scatter_matrices <- tapply(seq_len(n), y, function(i) {
+    (length(i) - 1) * cov(x[i, ])
+  })
+  Reduce("+", scatter_matrices) / n
+}
