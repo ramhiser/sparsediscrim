87b215c5b1319b433af807094054f4627f3ade17
John Ramey <johnramey@gmail.com>
2012-03-28 23:15:04 -0500

    Added function to parametric bootstrap with transform and inverse transform.
    I have added the 'boot_parametric' function to bootstrap from K
    populations parametrically with the option to transform and back
    transform following Parrish et al. (2009). The only available
    transformation currently is that of Yeo and Johnson (2000). Also, so
    that the sample covariance matrices are positive definite, I have added
    a shrinkage estimator from the RDA model given in Hastie et al.'s EoSL
    text.

---
+++ R/data-parametric-bootstrap.r
@@ -0,0 +1,144 @@
+#' Generates data from \code{K} multivariate data populations via a parametric
+#' bootsrapping technique with covariance matrix shrinkage.
+#'
+#' This function generates \code{K} multivariate data sets, where each
+#' data set is generated with a mean vector and a covariance matrix calculated
+#' from the data provided in \code{x}. The data are returned as a single matrix,
+#' \code{x}, along with a vector of class labels, \code{y}, that indicates class
+#' membership.
+#' 
+#' TODO: Define the covariance matrix, \eqn{\Sigma_k}.
+#' TODO: Define the shrinkage covariance matrix estimator.
+#'
+#' @export
+#' @param n vector of the sample sizes of each class to generate. The
+#' \code{length} of \code{n} should match the number of classes given in
+#' \code{y}.
+#' @param x matrix of observations with observations on the rows and features on
+#' the columns
+#' @param y vector of class labels for the observations (rows) in \code{x}.
+#' @param gamma numeric value between 0 and 1, inclusively. The value shrinks
+#' the sample covariance matrix for each class towards its diagonal with a weight
+#' given by \code{gamma}.
+#' @param transformation character. If \code{none} (default), no transformation is
+#' applied to data in \code{x} before sampling from the Multivariate Normal (MVN)
+#' distribution. If \code{Yeo-Johnson}, we first apply the Yeo-Johnson
+#' near-normality transformation to each column (that is, marginally) before
+#' generating data from the MVN distribution. After the data has been generated,
+#' we apply the inverse transformation to the generated data to attempt to
+#' restore the shape and scale of the underlying data.
+#' @return named list with elements:
+#' \itemize{
+#'   \item \code{x}: matrix of observations with \code{sum(n)} rows and \code{p}
+#' columns
+#'   \item \code{y}: vector of class labels that indicates class membership for
+#' each observation (row) in \code{x}.
+#' }
+#' @examples
+#' TODO
+boot_parametric <- function(n, x, y, gamma = 1,
+                            transformation = c("none", "Yeo-Johnson")) {
+  require('mvtnorm')
+  n <- as.integer(n)
+  x <- as.matrix(x)
+  y <- as.factor(y)
+  transformation <- match.arg(transformation)
+
+  if (gamma < 0 || gamma > 1) {
+    stop("The value of 'gamma' must be between 0 and 1, inclusively.")
+  }
+  if (length(y) != nrow(x)) {
+    stop("The length of 'y' must equal the number of rows of 'x'.")
+  }
+  if (length(n) != nlevels(y)) {
+    stop("The length of 'n' must equal the number of classes in 'y'.")
+  }
+
+  # If the Yeo-Johnson transformation is selected, we marginally estimate
+  # the power parameters 'lambda' for each class. We use the 'powerTransform'
+  # function from the 'car' package to estimate the values for 'lambda' and
+  # then use the 'yjPower' function from the same package to perform the
+  # transformation.
+  if (transformation == "Yeo-Johnson") {
+    yj_out <- tapply(seq_along(y), y, function(i) {
+      lambda <- powerTransform(x[i, ])$lambda
+      list(
+           lambda = lambda,
+           x = as.matrix(yjPower(x[i, ], lambda = lambda))
+      )
+    })
+    # The list 'yj_lambda' stores the transformation parameters for each class.
+    yj_lambda <- lapply(yj_out, function(z) z$lambda)
+
+    # The matrix 'x' contains the transformed data.
+    x <- do.call(rbind, lapply(yj_out, function(z) z$x))
+  }
+
+  xbars <- tapply(seq_along(y), y, function(i) {
+    colMeans(x[i, ])
+  })
+  covs_shrunken <- tapply(seq_along(y), y, function(i) {
+    cov_shrink_diag(x[i, ], gamma = gamma)
+  })
+  sample_sizes <- as.list(n)
+  
+  x <- mapply(function(n, mu, sigma) {
+      rmvnorm(n = n, mean = mu, sigma = sigma)
+    }, sample_sizes, xbars, covs_shrunken, SIMPLIFY = FALSE)
+  x <- unname(do.call(rbind, x))
+      
+  y <- mapply(function(n, label) {
+    rep.int(label, n)
+  }, sample_sizes, levels(y), SIMPLIFY = FALSE, USE.NAMES = FALSE)
+  y <- as.factor(do.call(c, y))
+  
+  # If the Yeo-Johnson transformation is selected, we now apply the inverse
+  # of the Yeo-Johnson transformation based on the estimated values of 'lambda'
+  # for each class. These values are stored in the list 'yj_lambda'.
+  if (transformation == "Yeo-Johnson") {
+    # The row indices for each class.
+    class_indices <- tapply(seq_along(y), y, identity)
+
+    # For each class, we apply the inverse of the Yeo-Johnson transformation to
+    # the generated data.
+    x <- mapply(function(i, lambda) {
+      apply(x[i, ], 1, yj_inverse, lambda = lambda)
+    }, class_indices, yj_lambda, SIMPLIFY = FALSE)
+    x <- t(do.call(cbind, x))
+  }
+
+  list(x = x, y = y)
+}
+
+#' Computes the inverse of Yeo-Johnson transformed random variate(s)
+#'
+#' TODO: Briefly describe Yeo-Johnson is generalization of Box-Cox
+#' TODO: Provide the formulas for Yeo-Johnson transformation (given in paper)
+#' TODO: Provide the inverse formulas for Yeo-Johnson (on whiteboard and iPhone)
+#' TODO: Cite Yeo-Johnson paper
+#' TODO: Reference the 'car' package to find the fitted 'lambda'
+#'
+#' @export
+#' @param y vector of random variate(s)
+#' @param lambda numeric Yeo-Johnson power transformation parameter value
+#' @param tol numeric tolerance value that determines numerical equality
+#' @return the inverse of the Yeo-Johnson random variate, \code{y}
+yj_inverse <- function(y, lambda, tol = sqrt(.Machine$double.eps)) {
+  if (y >= 0) {
+    # Case: lambda = 0 (numerically)
+    if (abs(lambda) < tol) {
+      x <- exp(y) - 1
+    } else { # Case: Lambda != 0
+      x <- (lambda * y + 1)^(1 / lambda) - 1
+    }
+  } else { # y is negative
+    # Case: lambda = 2 (numerically)
+    if (abs(lambda - 2) < tol) {
+      x <- 1 - exp(-y)
+    } else { # Case: Lambda != 2
+      x <- -(-(2 - lambda) * y + 1)^(1 / (2 - lambda)) + 1
+    }
+  }
+  x
+}
+yj_inverse <- Vectorize(yj_inverse, vectorize.args = c("y", "lambda"))
--- R/helper-cov-mle.r
+++ R/helper-cov-mle.r
@@ -32,3 +32,38 @@ cov_pool <- function(x, y) {
   })
   Reduce("+", scatter_matrices) / n
 }
+
+#' Computes a shrunken version of the maximum likelihood estimator for the
+#' sample covariance matrix under the assumption of multivariate normality.
+#'
+#' For a sample matrix, \code{x}, we compute the sample covariance matrix
+#' as the maximum likelihood estimator (MLE) of the population covariance
+#' matrix and shrink it towards its diagonal.
+#'
+#' Let \eqn{\widehat{\Sigma}} be the MLE of the covariance matrix \eqn{\Sigma}.
+#' Then, we shrink the MLE towards its diagonal by computing
+#' \deqn{\widehat{\Sigma}(\gamma) = \gamma \widehat{\Sigma} + (1 - \gamma) \widehat{\Sigma} \circ I_p},
+#' where \eqn{\circ} denotes the Hadamard product and \eqn{\gamma \in [0,1]}.
+#'
+#' For \eqn{\gamma < 1}, the resulting shrunken covariance matrix estimator is
+#' positive definite, and for \eqn{\gamma = 1}, we simply have the MLE, which can
+#' potentially be positive semidefinite (singular).
+#'
+#' The estimator given here is based on Section 18.3.1 of the text,
+#' "Elements of Statistical Learning."
+#'
+#' TODO: Cite the text.
+#'
+#' @export
+#' @param x data matrix with \code{n} observations and \code{p} feature vectors
+#' @param gamma the shrinkage parameter. Must be between 0 and 1, inclusively.
+#' By default, the shrinkage parameter is 1, which simply yields the MLE.
+#' @return shrunken sample covariance matrix of size \eqn{p \times p}
+cov_shrink_diag <- function(x, gamma = 1) {
+  if (gamma < 0 || gamma > 1) {
+    stop("The value of 'gamma' must be between 0 and 1, inclusively.")
+  }
+  cov_x <- diagdiscrim:::cov_mle(x)
+  gamma * cov_x + (1 - gamma) * diag(diag(cov_x))
+}
+
