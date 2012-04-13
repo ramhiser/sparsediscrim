0fc06f18179c25f0217ba88aef51010e83337319
John Ramey <johnramey@gmail.com>
2012-04-13 14:36:13 -0700

    Updated the Bhattacharyya distance dimension reduction method.
    Rather than using the ad hoc percentage change option with the
    Bhattacharyya distance dimension reduction approach, we are now using
    the 'q' transformed features with the largest Bhattacharyya distances.
    This approach is more straightforward and follows the Fukunaga text
    more closely for the more general positive semidefinite case.

--- R/bhattacharyya.r
+++ R/bhattacharyya.r
@@ -39,15 +39,20 @@ bhatta_simdiag <- function(x, y, q = NULL, shrink = TRUE) {
 
   # Selects the first 'q' columns of 'x'. By default, we keep all of the columns
   # of 'x'.
-  if (is.null(q)) {
-    q <- ncol(x)
+  if (!is.null(q)) {
+    q <- as.integer(q)
+    if (q < 1 || q > ncol(x)) {
+      stop("The value of 'q' must be between 1 and 'ncol(x)', inclusively.")
+    }
+    x <- as.matrix(x[, seq_len(q)])
   }
-  x <- x[, seq_len(q)]
 
   # Partitions the matrix 'x' into the data for each class.
   class_labels <- levels(y)
-  x1 <- x[which(y == class_labels[1]), ]
-  x2 <- x[which(y == class_labels[2]), ]
+  # The calls to 'as.matrix' are to maintain a matrix form if the number of
+  # columns of 'x' is 1.
+  x1 <- as.matrix(x[which(y == class_labels[1]), ])
+  x2 <- as.matrix(x[which(y == class_labels[2]), ])
 
   # Calculates the MLEs for the population means and (diagonal) covariance
   # matrices for each class.
@@ -87,14 +92,12 @@ bhatta_simdiag <- function(x, y, q = NULL, shrink = TRUE) {
 #' given in \code{y} for the dimensions ranging from 2 to \code{ncol(x)}.
 #' We assume that the matrix \code{x} has been simultaneously diagonalized.
 #'
-#' We wish to determine the value of \code{q}, such that the percentage change
-#' in the Bhattacharyya distance for the first \code{q} columns and the
-#' Bhattacharyya distance for the first \code{q + 1} columns is less than
-#' \code{tol}. The selected value of \code{q} is the dimension to which the data
-#' matrix, \code{x}, is reduced. If are no cases where the percentage change is
-#' less than \code{tol}, then we select the reduced dimension to be the
-#' \code{ncol(x)}. Notice that our procedure is similar to that of the 'elbow
-#' criterion' often employed with a scree plot in a Principal Components
+#' We wish to determine the \code{q} largest Bhattacharyya distances from the
+#' \code{p} features in \code{x}. We assume that the class covariance matrices
+#' for both classes given in \code{y} are diagonal. The selected value of
+#' \code{q} is the dimension to which the data matrix, \code{x}, is reduced.
+#' Notice that our procedure is similar to the reduced dimension selection
+#' technique often employed with a scree plot in a Principal Components
 #' Analysis (PCA). Our approach allows us to generalize the PCA dimension
 #' reduction technique, accordingly.
 #'
@@ -110,31 +113,36 @@ bhatta_simdiag <- function(x, y, q = NULL, shrink = TRUE) {
 #' @export
 #' @param x data matrix with \code{n} observations and \code{p} feature vectors
 #' @param y class labels for observations (rows) in \code{x}
-#' @param tol tolerance value. If the percentage change in the Bhattacharyya
-#' distance for a given value of \code{q} and \code{q + 1} is less than
-#' \code{tol}, then we select the value of \code{q} as the optimal value. If
-#' there are no cases where the percentage change is less than \code{tol}, then
-#' we select the reduced dimension to be the \code{ncol(x)}.
+#' @param bhatta_prop threshold value for the maximum cumulative proportion of
+#' the Bhattacharyya distance. We use the threshold to determine the reduced
+#' dimension \code{q}. When paired with the SimDiag method, the Bhattacharyya
+#' approach to dimension reduction generalizes the scree plot idea that is often
+#' utilized with Principal Components Analysis.
 #' @param shrink By default, (if \code{TRUE}), we shrink each covariance matrix
 #' with the MDEB covariance matrix estimator. Otherwise, no shrinkage is applied.
 #' @return a list containing:
-#'	the selected value of \code{q}
-#'  the estimated Bhattacharyya distance between the two classes for each
-#' candidate value of \code{q} from 2 to \code{ncol(x)}.
-#'  the percentage change between the Bhattacharyya distances for the sequence of
-#' candidate values of \code{q}.
-dimred_bhatta_simdiag <- function(x, y, shrink = TRUE, tol = 1e-3) {
-  bhatta_q <- sapply(seq.int(2, ncol(x)), function(q) {
-    bhatta_simdiag(x = x, y = y, q = q, shrink = shrink)
+#' \itemize{
+#'   \item \code{q}: the reduced dimension determined via \code{bhatta_pct}
+#'   \item \code{bhatta_dist}: the estimated Bhattacharyya distance between the
+#'   two classes for each dimension in the transformed space.
+#'   \item \code{bhatta_dist_rank}: the indices of the columns of \code{x}
+#'   corresponding to the \code{q} largest Bhattacharyya distances.
+#'   \item \code{bhatta_cumprop}: the cumulative proportion of the sorted
+#'   Bhattacharyya distances for each column given in \code{x}.
+#' }
+dimred_bhatta_simdiag <- function(x, y, bhatta_prop = 0.9, shrink = TRUE) {
+  bhatta_dist <- sapply(seq_len(ncol(x)), function(j) {
+    bhatta_simdiag(x[, j], y, shrink = shrink)
   })
-  bhatta_pct_change <- diagdiscrim:::pct_change(bhatta_q)
+  sorted_bhatta_dist <- sort(bhatta_dist, decreasing = TRUE)
+  bhatta_cumprop <- cumsum(sorted_bhatta_dist) / sum(sorted_bhatta_dist)
 
-  small_pct_change <- which(bhatta_pct_change < tol)
-  if (length(small_pct_change) == 0) {
-    q <- ncol(x)
-  } else {
-    q <- small_pct_change[1] + 1
-  }
+  q <- sum(bhatta_cumprop <= bhatta_prop)
+  bhatta_dist_rank <- order(bhatta_dist, decreasing = TRUE)[seq_len(q)]
 
-  list(q = q, bhattacharyya = bhatta_q, pct_change = bhatta_pct_change)
+  list(q = q, bhatta_dist = bhatta_dist, bhatta_dist_rank = bhatta_dist_rank,
+       bhatta_cumprop = bhatta_cumprop)
 }
+
+
+
