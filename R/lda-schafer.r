3d607021ac516cd9ef848d1472fcebcacfa859c4
John Ramey <johnramey@gmail.com>
2014-03-29 22:33:04 -0500

    Added the lda_schafer classifier along with unit tests
    The actual classifier: Linear Discriminant Analysis (LDA) using the
    covariance matrix estimator from Schafer and Strimmer (2005)

--- R/lda-schafer.r
+++ R/lda-schafer.r
@@ -1,14 +1,13 @@
-#' Linear Discriminant Analysis (LDA) with the Moore-Penrose Pseudo-Inverse
+#' Linear Discriminant Analysis using the Schafer-Strimmer Covariance Matrix
+#' Estimator
 #'
 #' Given a set of training data, this function builds the Linear Discriminant
 #' Analysis (LDA) classifier, where the distributions of each class are assumed
-#' to be multivariate normal and share a common covariance matrix.
-#' When the pooled sample covariance matrix is singular, the linear discriminant
-#' function is incalculable. A common method to overcome this issue is to
-#' replace the inverse of the pooled sample covariance matrix with the
-#' Moore-Penrose pseudo-inverse, which is unique and always exists. Note that
-#' when the pooled sample covariance matrix is nonsingular, it is equal to the
-#' pseudo-inverse.
+#' to be multivariate normal and share a common covariance matrix. When the
+#' pooled sample covariance matrix is singular, the linear discriminant function
+#' is incalculable. This function replaces the inverse of pooled sample
+#' covariance matrix with an estimator proposed by Schafer and Strimmer
+#' (2005). The estimator is calculated via \code{\link[corpcor]{invcov.shrink}}.
 #'
 #' The matrix of training observations are given in \code{x}. The rows of \code{x}
 #' contain the sample observations, and the columns contain the features for each
@@ -28,6 +27,7 @@
 #' as the number of classes in \code{y}. The \code{prior} probabilties should be
 #' nonnegative and sum to one.
 #'
+#' @importFrom corpcor invcov.shrink
 #' @export
 #'
 #' @param x matrix containing the training data. The rows are the sample
@@ -35,33 +35,45 @@
 #' @param y vector of class labels for each training observation
 #' @param prior vector with prior probabilities for each class. If NULL
 #' (default), then equal probabilities are used. See details.
-#' @return \code{lda_pseudo} object that contains the trained lda_pseudo
-#' classifier
+#' @param ... additional arguments passed to
+#' \code{\link[corpcor]{invcov.shrink}}
+#' @return \code{lda_schafer} object that contains the trained classifier
 #' @examples
 #' n <- nrow(iris)
 #' train <- sample(seq_len(n), n / 2)
-#' lda_pseudo_out <- lda_pseudo(Species ~ ., data = iris[train, ])
-#' predicted <- predict(lda_pseudo_out, iris[-train, -5])$class
+#' lda_schafer_out <- lda_schafer(Species ~ ., data = iris[train, ])
+#' predicted <- predict(lda_schafer_out, iris[-train, -5])$class
 #'
-#' lda_pseudo_out2 <- lda_pseudo(x = iris[train, -5], y = iris[train, 5])
-#' predicted2 <- predict(lda_pseudo_out2, iris[-train, -5])$class
+#' lda_schafer_out2 <- lda_schafer(x = iris[train, -5], y = iris[train, 5])
+#' predicted2 <- predict(lda_schafer_out2, iris[-train, -5])$class
 #' all.equal(predicted, predicted2)
-lda_pseudo <- function(x, ...) {
-  UseMethod("lda_pseudo")
+#' @references Schafer, J., and Strimmer, K. (2005). "A shrinkage approach to
+#' large-scale covariance estimation and implications for functional genomics,"
+#' Statist. Appl. Genet. Mol. Biol. 4, 32.
+lda_schafer <- function(x, ...) {
+  UseMethod("lda_schafer")
 }
 
-#' @rdname lda_pseudo
-#' @method lda_pseudo default
-#' @S3method lda_pseudo default
-lda_pseudo.default <- function(x, y, prior = NULL) {
+#' @rdname lda_schafer
+#' @method lda_schafer default
+#' @S3method lda_schafer default
+lda_schafer.default <- function(x, y, prior = NULL, ...) {
   x <- as.matrix(x)
   y <- as.factor(y)
 
-  obj <- regdiscrim_estimates(x = x, y = y, prior = prior, cov = TRUE)
+  obj <- regdiscrim_estimates(x = x, y = y, prior = prior, cov = FALSE)
 
-  # Creates an object of type 'lda_pseudo' and adds the 'match.call' to the object
+  # Centers the data matrix from each class and then estimates the lda_schafer
+  # precision matrix
+  x_centered <- tapply(seq_along(y), y, function(i) {
+    scale(x[i, ], center = TRUE, scale = FALSE)
+  })
+  x_centered <- do.call(rbind, x_centered)
+  obj$cov_inv <- invcov.shrink(x_centered, verbose = FALSE, ...)
+
+  # Creates an object of type 'lda_schafer' and adds the 'match.call' to the object
   obj$call <- match.call()
-  class(obj) <- "lda_pseudo"
+  class(obj) <- "lda_schafer"
 	
 	obj
 }
@@ -71,10 +83,10 @@ lda_pseudo.default <- function(x, y, prior = NULL) {
 #' (non-factor) discriminators.
 #' @param data data frame from which variables specified in \code{formula} are
 #' preferentially to be taken.
-#' @rdname lda_pseudo
-#' @method lda_pseudo formula
-#' @S3method lda_pseudo formula
-lda_pseudo.formula <- function(formula, data, prior = NULL) {
+#' @rdname lda_schafer
+#' @method lda_schafer formula
+#' @S3method lda_schafer formula
+lda_schafer.formula <- function(formula, data, prior = NULL) {
   # The formula interface includes an intercept. If the user includes the
   # intercept in the model, it should be removed. Otherwise, errors and doom
   # happen.
@@ -86,24 +98,24 @@ lda_pseudo.formula <- function(formula, data, prior = NULL) {
   x <- model.matrix(attr(mf, "terms"), data = mf)
   y <- model.response(mf)
 
-  est <- lda_pseudo.default(x = x, y = y, prior = prior)
+  est <- lda_schafer.default(x = x, y = y, prior = prior)
   est$call <- match.call()
   est$formula <- formula
   est
 }
 
-#' Outputs the summary for a lda_pseudo classifier object.
+#' Outputs the summary for a lda_schafer classifier object.
 #'
-#' Summarizes the trained lda_pseudo classifier in a nice manner.
+#' Summarizes the trained lda_schafer classifier in a nice manner.
 #'
 #' @keywords internal
 #' @param x object to print
 #' @param ... unused
-#' @rdname lda_pseudo
-#' @method print lda_pseudo
-#' @S3method print lda_pseudo
+#' @rdname lda_schafer
+#' @method print lda_schafer
+#' @S3method print lda_schafer
 #' @export
-print.lda_pseudo <- function(x, ...) {
+print.lda_schafer <- function(x, ...) {
   cat("Call:\n")
   print(x$call)
   cat("Sample Size:\n")
@@ -117,43 +129,41 @@ print.lda_pseudo <- function(x, ...) {
 }
 
 #' Predicts of class membership of a matrix of new observations using Linear
-#' Discriminant Analysis (LDA) with the Moore-Penrose Pseudo-Inverse
+#' Discriminant Analysis (LDA) using the Schafer-Strimmer Covariance Matrix
+#' Estimator
 #'
 #' The Linear Discriminant Analysis (LDA) classifier involves the assumption
 #' that the distributions of each class are assumed to be multivariate normal
 #' and share a common covariance matrix. When the pooled sample covariance
-#' matrix is singular, the linear discriminant function is incalculable. A
-#' common method to overcome this issue is to replace the inverse of the pooled
-#' sample covariance matrix with the Moore-Penrose pseudo-inverse, which is
-#' unique and always exists. Note that when the pooled sample covariance matrix
-#' is nonsingular, it is equal to the pseudo-inverse.
+#' matrix is singular, the linear discriminant function is incalculable. Here,
+#' the inverse of the pooled sample covariance matrix is replaced with an
+#' estimator from Schafer and Strimmer (2005).
 #' 
-#' @rdname lda_pseudo
-#' @method predict lda_pseudo
-#' @S3method predict lda_pseudo
+#' @rdname lda_schafer
+#' @method predict lda_schafer
+#' @S3method predict lda_schafer
 #' @export
 #'
-#' @param object trained lda_pseudo object
+#' @references Schafer, J., and Strimmer, K. (2005). "A shrinkage approach to
+#' large-scale covariance estimation and implications for functional genomics,"
+#' Statist. Appl. Genet. Mol. Biol. 4, 32.
+#' @param object trained lda_schafer object
 #' @param newdata matrix of observations to predict. Each row corresponds to a
 #' new observation.
 #' @param ... additional arguments
 #' @return list predicted class memberships of each row in newdata
-predict.lda_pseudo <- function(object, newdata, ...) {
-	if (!inherits(object, "lda_pseudo"))  {
-		stop("object not of class 'lda_pseudo'")
+predict.lda_schafer <- function(object, newdata) {
+	if (!inherits(object, "lda_schafer"))  {
+		stop("object not of class 'lda_schafer'")
 	}
 	if (is.vector(newdata)) {
     newdata <- matrix(newdata, nrow = 1)
   }
 
-  # Calculates the Moore-Penrose pseudo inverse based on the pooled sample
-  # covariance matrix
-  cov_pseudoinv <- ginv(object$cov_pool)
-
   # Calculates the discriminant scores for each test observation
 	scores <- apply(newdata, 1, function(obs) {
 		sapply(object$est, function(class_est) {
-			with(class_est, quadform(cov_pseudoinv, obs - xbar) + log(prior))
+			with(class_est, quadform(object$cov_inv, obs - xbar) + log(prior))
 		})
 	})
 	
