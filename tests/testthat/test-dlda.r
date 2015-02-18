2cbb06b286325d8ca99afead25ab00c192e9c971
John Ramey <johnramey@gmail.com>
2015-02-17 18:48:30 -0600

    Test for DLDA posterior probs.

--- tests/testthat/test-dlda.r
+++ tests/testthat/test-dlda.r
@@ -10,12 +10,14 @@ test_that("The DLDA classifier works properly on the iris data set", {
   n <- nrow(iris)
   train <- sample(seq_len(n), n / 2)
   dlda_out <- dlda(Species ~ ., data = iris[train, ])
-  predicted <- predict(dlda_out, iris[-train, -5])$class
+  predicted <- predict(dlda_out, iris[-train, -5])
 
   dlda_out2 <- dlda(x = iris[train, -5], y = iris[train, 5])
-  predicted2 <- predict(dlda_out2, iris[-train, -5])$class
+  predicted2 <- predict(dlda_out2, iris[-train, -5])
 
   # Tests that the same labels result from the matrix and formula versions of
   # the DLDA classifier
-  expect_equal(predicted, predicted2)
+  expect_equal(predicted$class, predicted2$class)
+
+  expect_is(predicted$posterior, "matrix")
 })
